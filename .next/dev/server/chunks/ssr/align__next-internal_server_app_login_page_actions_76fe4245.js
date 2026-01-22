804134a5c58fbcfc91f2137c082ab066d0fc7379
adeyemichaek <ayobami732000@gmail.com>
2026-01-22 20:10:33 +0100

    Remove .next from git tracking

--- .next/dev/server/chunks/ssr/align__next-internal_server_app_login_page_actions_76fe4245.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/align/.next-internal/server/app/login/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=align__next-internal_server_app_login_page_actions_76fe4245.js.map
--- .next/dev/static/chunks/00561_next_dist_client_components_builtin_global-error_6dd980e0.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/00561_next_dist_b79a42b7._.js"
-],
-    source: "dynamic"
-});
